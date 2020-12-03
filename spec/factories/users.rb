ac8e03a4c5625a0e318da30a83a4a24e87780817
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-03 19:18:47 +0900

    【修正】バリデーション　パスワード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { 'test@example' }
-    password              { '000000' }
+    password              { 'a000000' }
     password_confirmation { password }
     last_name             { 'あ' }
     first_name            { 'い' }
