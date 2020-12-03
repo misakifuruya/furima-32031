ca2e448731ed2b20ae0f51c4cd74934ad402f5a1
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-03 18:46:51 +0900

    テストコード

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # before_action :authenticate_user! ログイン強制送還
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
