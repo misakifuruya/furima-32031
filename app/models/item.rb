8f9d64843efdd488d9a94093dc1cb632ff616ee3
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-07 21:25:10 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area, :category, :day, :shipping_cost, :status
+
+  validates :area_id, :days_id, :shipping_cost_id, :category_id, :status_id,  numericality: { other_than: 1 } 
+
+  validates :image, :title, :user, :explanation, :price, presence: true
+
 end
