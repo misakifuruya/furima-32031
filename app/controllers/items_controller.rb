7c53229c40c97ab64d4cab6fb6ce6f30fa07460c
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-12 22:05:11 +0900

    error

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,17 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index]
+
   def index
-    #  @items = Item.all
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
 
+  def
+  end
+  
 
 end
