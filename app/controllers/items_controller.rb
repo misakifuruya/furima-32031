65d1b9ade6918f0fd761d521fb2db13bd5f92764
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-14 17:35:45 +0900

    ビフォーアクション　コントローラー追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -31,7 +32,7 @@ class ItemsController < ApplicationController
 
     if @item.update(item_params)
       redirect_to item_path
-    
+
     else
       render :edit
     end
@@ -41,5 +42,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:title, :user, :explanation, :category_id, :status_id, :shipping_cost_id, :area_id, :schedule_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
