6bce576fec45bce48c8c686b0478b65cfdfb89ac
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-15 21:23:24 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   
-  resources :item do
+  resources :items do
   resources :orders, only:[:create, :index]
   end
 end
