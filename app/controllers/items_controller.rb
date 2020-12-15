287c2abc9e2572948045c675ea2f9e4b9c936607
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-15 17:29:43 +0900

    コントローラー　destroy追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update, :move_to_index]
+  before_action :set_item, only: [:edit, :show, :update, :move_to_index, :destroy]
   before_action :move_to_index, only: [:edit, :update]
-  
 
   def index
     @items = Item.order('created_at DESC')
@@ -28,13 +27,16 @@ class ItemsController < ApplicationController
   end
 
   def update
-
     if @item.update(item_params)
       redirect_to item_path
 
     else
       render :edit
     end
+  end
+
+  def destroy
+    render :index if @item.destroy
   end
 
   private
@@ -50,6 +52,4 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless current_user.id == @item.user_id
   end
-
-
 end
