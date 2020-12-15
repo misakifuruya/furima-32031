a5cdd06c7acebb607d26c33a772d87e3fba57415
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-15 21:23:38 +0900

    テーブル、モデル作成

---
+++ db/migrate/20201215121422_create_addresses.rb
@@ -0,0 +1,15 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string     :postal_code,    null: false
+      t.string     :area,           null: false
+      t.integer    :prefectures_id, null: false
+      t.string     :city,           null: false
+      t.string     :district,       null: false
+      t.string     :phone_number,   null: false
+      t.references :order,          null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
