d8a56aa06205ac1f03f6183af9e9d12caf5af036
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-16 17:07:57 +0900

    バリデーション設定

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,9 +3,13 @@ class OrderAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :area_id, :district, :phone_number
 
   validates :area_id, numericality: { other_than: 1 }
-  validates :user_id, :item_id,  presence: true
-
-  validates :postal_code, :area_id, :prefectures, :city, :phone_number,  presence: true
+  validates :user_id,          presence: true
+  validates :item_id,          presence: true
+  validates :postal_code,      presence: true
+  validates :area_id,          presence: true
+  validates :prefecture,       presence: true
+  validates :city,             presence: true
+  validates :phone_number,     presence: true
 
 
    validates :phone_number, format: { with: /\A[0-9]+\z/i }
@@ -13,8 +17,9 @@ class OrderAddress
    validates :district, format: { with: /\A[ぁ-んァ-ン一-龥]/}
 
 def save
-  order = Order.create(user_id: user.id, item_id: item.id)
-  Address.create(postal_code: postal_code, prefecture: prefecture, city: city, area_id: area.id, district: district, phone_number: phone_number, order_id: order.id)
+  binding.pry
+  order = Order.create(user_id: user_id, item_id: item_id)
+  Address.create(postal_code: postal_code, prefecture: prefecture, city: city, area_id: area_id, district: district, phone_number: phone_number, order_id: order.id)
 
 end
 
