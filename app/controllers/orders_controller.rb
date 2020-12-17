e435775eaf42e96fcaddbff93ec462bdde79bb52
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-17 17:51:36 +0900

    コントローラー条件分岐購入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
+    redirect_to action: :index if current_user.id == @item.user_id
     @order_address = OrderAddress.new
+
+  
+    if @item.order != nil
+      redirect_to root_path
+    end
   end
 
   def create
-    @item = Item.find(params[:item_id])
 
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
@@ -18,10 +24,14 @@ class OrdersController < ApplicationController
   end
 
   private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 
   def order_params
     params.require(:order_address).permit(:postal_code, :area_id, :city, :prefecture, :district, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
 
   def pay_item
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
