8c0613176a2f50d29e49e51bf9358210d6a3251a
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-17 17:51:55 +0900

    バリデーション変更

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,20 +2,18 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :area_id, :district, :phone_number, :token
 
+
+  with_options presence: true do
   validates :area_id, numericality: { other_than: 1 }
-  validates :user_id,          presence: true
-  validates :item_id,          presence: true
-  validates :postal_code,      presence: true
-  validates :area_id,          presence: true
-  validates :prefecture,       presence: true
-  validates :city,             presence: true
-  validates :phone_number,     presence: true
-
-  validates :token, presence: true
-
   validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
   validates :phone_number, format: { with: /[0-9\d]{11,}/i }
   validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+   end
+   validates :user_id,          presence: true
+   validates :item_id,          presence: true
+   validates :prefecture,       presence: true
+ 
+   validates :token, presence: true
   validates :district, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
 
   def save
