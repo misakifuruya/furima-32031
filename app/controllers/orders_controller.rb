d558dadf2e997a05e1d4441eb014f1d036775a54
misakifuruya <m2s4k.msk24@gmail.com>
2020-12-17 13:05:19 +0900

    コンロローラーカード機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,10 +7,10 @@ class OrdersController < ApplicationController
 
   def create
     @item = Item.find(params[:item_id])
-
   
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -22,9 +22,19 @@ class OrdersController < ApplicationController
     private
 
     def order_params
-      params.require(:order_address).permit(:postal_code, :area_id, :city, :prefecture, :district, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-    
+      params.require(:order_address).permit(:postal_code, :area_id, :city, :prefecture, :district, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+      Payjp::Charge.create(
+        amount: @item[:price],  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+  end
+
+
 end
 
 
